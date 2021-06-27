6fab54777f922827489340c1864e7b5aad9e9b04
Eduardo <ex.romeroma@gmail.com>
2021-06-27 13:04:29 +0200

    dfs recursive implementation

--- index.js
+++ index.js
@@ -19,36 +19,87 @@ class Cell {
         this.y = y;
         this.w = w;
         this.h = h;
-        this.walls = [];
+        this.visited = false;
+        this.walls = {
+            bottom: true,
+            right: true,
+            top: true,
+            left: true
+        };
+    }
+
+    findIndex(x, y) {
+        if (x < 0 || y < 0 || x > cols < 1 || y > rows - 1) return -1;
+        return x + y * cols;
+    }
+
+    isNeighborVisited() {
+        const neighbors = [
+            grid[this.findIndex(i, j + 1)], // bottom neighbor
+            grid[this.findIndex(i + 1, j)], // right neighbor
+            grid[this.findIndex(i, j - 1)], // top neighbor
+            grid[this.findIndex(i - 1, j)]  // left neighbor
+        ].filter(neighbor => !neighbor.visited);
+
+        if (neighbors.length) {
+            const rand = Math.floor(Math.random() * neighbors.length);
+            return neighbors[rand];
+        } else {
+            return undefined;
+        }
+        // recursive implementation
+        // 1. Given a current cell as a parameter,
+        // 2. Mark the current cell as visited
+        // 3. While the current cell has any unvisited neighbour cells
+        // Neighbors
+        // 
+        // 4. Choose one of the unvisited neighbours
+        // 5. Remove the wall between the current cell and the chosen cell
+        // 6. Invoke the routine recursively for a chosen cell
     }
 
     show() {
         ctx.strokeStyle = 'white';
         ctx.beginPath();
 
+        if (this.visited) {
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.fillStyle = 'yellow';
+            ctx.fillRect(this.x, this.y, this.w, this.h);
+        }
+
         // Bottom
-        ctx.moveTo(this.x, this.y + this.h);
-        ctx.lineTo(this.x + this.w, this.y + this.h);
-        ctx.stroke();
+        if (this.walls.bottom) {
+            ctx.moveTo(this.x, this.y + this.h);
+            ctx.lineTo(this.x + this.w, this.y + this.h);
+            ctx.stroke();
+        }
 
         // Right
-        ctx.moveTo(this.x + this.w, this.y + this.h);
-        ctx.lineTo(this.x + this.w, this.y);
-        ctx.stroke();
+        if (this.walls.right) {
+            ctx.moveTo(this.x + this.w, this.y + this.h);
+            ctx.lineTo(this.x + this.w, this.y);
+            ctx.stroke();
+        }
 
         // Top
-        ctx.moveTo(this.x + this.w, this.y);
-        ctx.lineTo(this.x, this.y)
-        ctx.stroke();
+        if (this.walls.top) {
+            ctx.moveTo(this.x + this.w, this.y);
+            ctx.lineTo(this.x, this.y)
+            ctx.stroke();
+        }
 
         // Left
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(this.x, this.y + this.h);
-        ctx.stroke();
+        if (this.walls.left) {
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(this.x, this.y + this.h);
+            ctx.stroke();
+        }
     }
 
     drawSide(rand) {
-        ctx.strokeStyle = 'white';
+        ctx.strokeStyle = '#565656';
         ctx.lineWidth = 2
         ctx.beginPath();
         switch (rand) {
@@ -77,7 +128,7 @@ class Cell {
 
 const drawGrid = () => {
     for (let i = 0; i < grid.length; i++) {
-        grid[i].drawSide(Math.floor(Math.random() * 4));
+        grid[i].show();
     }
 }
 
@@ -90,6 +141,17 @@ const fillGrid = () => {
     }
 }
 
-fillGrid();
-drawGrid();
+
+const init = () => {
+    fillGrid();
+
+    grid[0].visited = true;
+
+    drawGrid();
+}
+
+init();
+
+
+
 
